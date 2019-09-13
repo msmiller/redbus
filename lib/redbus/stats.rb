3b10c83a9744b5d5012b881e2028bb94016e7a3d
msmiller <mark.s.miller@gmail.com>
2019-09-13 12:12:12 -0700

    stats

--- lib/redbus/stats.rb
+++ lib/redbus/stats.rb
@@ -2,7 +2,7 @@
 # @Author: msmiller
 # @Date:   2019-09-12 20:42:13
 # @Last Modified by:   msmiller
-# @Last Modified time: 2019-09-12 21:01:45
+# @Last Modified time: 2019-09-13 12:04:32
 #
 # Copyright (c) 2017-2018 Sharp Stone Codewerks / Mark S. Miller
 
@@ -11,9 +11,40 @@
 # The data is housed as:
 #
 # stats:{channel}:year:month:[published, processed, failed]
+#
+# NOTE: I'm not using the Redis hash here since we're mainly concerned with
+# storage speed, not with dumping results for some console.
 
 module Redbus
   class Stats
+
+    BUCKET_NAMES = [ 'published', 'processed', 'failed' ]
+
+    # Bump a counter for 'published', 'processed', or 'failed'
+    def self.bump(channel, bucket)
+      yr = Date.today.year
+      mo = Date.today.month
+      stats_key = "stats:#{channel}:#{bucket}:#{yr}:#{mo}"
+      # ap stats_key
+      $redis.incr(stats_key)
+    end
+
+    def self.counts_for(channel)
+      count_keys = $redis.keys( "stats:#{channel}:*" )
+      result = {}
+      count_keys.each do |k|
+        ignore1,ignore2,bucket,yr,mo = k.split(":")
+        result[bucket] ||= {}
+        result[bucket][yr.to_i] ||= {}
+        result[bucket][yr.to_i][mo.to_i] = $redis.get(k).to_i
+      end
+      return result
+    end
+
+    def self.clear(channel)
+      count_keys = $redis.keys( "stats:#{channel}:*" )
+      count_keys.each { |k| $redis.del(k) }
+    end
 
   end
 end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -8,7 +8,48 @@ RSpec.describe Redbus::Stats do
 
   context "stats" do
 
+    it "can bump a count" do
+      x = Redbus::Stats.bump( "@test1", "published" )
+      y = Redbus::Stats.bump( "@test1", "processed" )
+      y = Redbus::Stats.bump( "@test1", "processed" )
+      z = Redbus::Stats.bump( "@test1", "failed" )
+      z = Redbus::Stats.bump( "@test1", "failed" )
+      z = Redbus::Stats.bump( "@test1", "failed" )
+      expect(x).to eq(1)
+      expect(y).to eq(2)
+      expect(z).to eq(3)
+    end
+
+    it "can clear channel stats" do
+      count_keys = $redis.keys( "stats:*" )
+      expect(count_keys.length).to eq(3)
+      puts "    (Clear data left from prior test ...)"
+      Redbus::Stats.clear( "@test1" )
+      count_keys = $redis.keys( "stats:*" )
+      expect(count_keys.length).to eq(0)
+    end
+
+    it "can gather stats" do
+      load_some_data
+      yr = Date.today.year
+      mo = Date.today.month
+      counts = Redbus::Stats.counts_for( "@test1" )
+      expect(counts.length).to eq(3)
+      expect(counts["published"][yr][mo]).to eq(1)
+      expect(counts["processed"][yr][mo]).to eq(2)
+      expect(counts["failed"][yr][mo]).to eq(3)
+    end
+
   end # stats
+
+  def load_some_data
+    Redbus::Stats.bump( "@test1", "published" )
+    Redbus::Stats.bump( "@test1", "processed" )
+    Redbus::Stats.bump( "@test1", "processed" )
+    Redbus::Stats.bump( "@test1", "failed" )
+    Redbus::Stats.bump( "@test1", "failed" )
+    Redbus::Stats.bump( "@test1", "failed" )
+  end
 
 end
 
