16b4914c26757175d461354a4431aec1ccd67be0
msmiller <mark.s.miller@gmail.com>
2019-10-31 18:18:36 -0700

    stats working

--- lib/redbus/stats.rb
+++ lib/redbus/stats.rb
@@ -2,7 +2,7 @@
 # @Author: msmiller
 # @Date:   2019-09-12 20:42:13
 # @Last Modified by:   msmiller
-# @Last Modified time: 2019-10-29 16:01:39
+# @Last Modified time: 2019-10-31 18:12:33
 #
 # Copyright (c) Sharp Stone Codewerks / Mark S. Miller
 
@@ -17,36 +17,34 @@
 
 require 'date'
 
-module Redbus
-  class Stats
+class RedisBus
 
     BUCKET_NAMES = [ 'published', 'processed', 'failed' ]
 
     # Bump a counter for 'published', 'processed', or 'failed'
-    def self.bump(channel, bucket)
+    def bump(channel, bucket)
       yr = Date.today.year
       mo = Date.today.month
       stats_key = "stats:#{channel}:#{bucket}:#{yr}:#{mo}"
       # ap stats_key
-      $busredis.incr(stats_key)
+      self.busredis.incr(stats_key)
     end
 
-    def self.counts_for(channel)
-      count_keys = $busredis.keys( "stats:#{channel}:*" )
+    def counts_for(channel)
+      count_keys = self.busredis.keys( "stats:#{channel}:*" )
       result = {}
       count_keys.each do |k|
         ignore1,ignore2,bucket,yr,mo = k.split(":")
         result[bucket] ||= {}
         result[bucket][yr.to_i] ||= {}
-        result[bucket][yr.to_i][mo.to_i] = $busredis.get(k).to_i
+        result[bucket][yr.to_i][mo.to_i] = self.busredis.get(k).to_i
       end
       return result
     end
 
-    def self.clear(channel)
-      count_keys = $busredis.keys( "stats:#{channel}:*" )
-      count_keys.each { |k| $busredis.del(k) }
+    def clear(channel)
+      count_keys = self.busredis.keys( "stats:#{channel}:*" )
+      count_keys.each { |k| self.busredis.del(k) }
     end
 
-  end
 end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -1,31 +1,37 @@
 require 'redis'
 require 'awesome_print'
 
-RSpec.describe Redbus::Stats do
+RSpec.describe RedisBus do
 
   before :each do
+    # In this case we don't want to clear the bus in between ever time
+    @yaml_file = 'redbus_topology.yml'
+    @endpoint = 'test1'
+    @current_redbus = RedisBus.new(@endpoint, @yaml_file)
   end
 
   context "stats" do
 
     it "can bump a count" do
-      x = Redbus::Stats.bump( "@test1", "published" )
-      y = Redbus::Stats.bump( "@test1", "processed" )
-      y = Redbus::Stats.bump( "@test1", "processed" )
-      z = Redbus::Stats.bump( "@test1", "failed" )
-      z = Redbus::Stats.bump( "@test1", "failed" )
-      z = Redbus::Stats.bump( "@test1", "failed" )
+      @current_redbus.busredis.flushall
+      @current_redbus.busredis.flushdb
+      x = @current_redbus.bump( "@test1", "published" )
+      y = @current_redbus.bump( "@test1", "processed" )
+      y = @current_redbus.bump( "@test1", "processed" )
+      z = @current_redbus.bump( "@test1", "failed" )
+      z = @current_redbus.bump( "@test1", "failed" )
+      z = @current_redbus.bump( "@test1", "failed" )
       expect(x).to eq(1)
       expect(y).to eq(2)
       expect(z).to eq(3)
     end
 
     it "can clear channel stats (Use data left from prior test)" do
-      count_keys = $busredis.keys( "stats:*" )
+      count_keys = @current_redbus.busredis.keys( "stats:*" )
       expect(count_keys.length).to eq(3)
       # Clear data left from prior test
-      Redbus::Stats.clear( "@test1" )
-      count_keys = $busredis.keys( "stats:*" )
+      @current_redbus.clear( "@test1" )
+      count_keys = @current_redbus.busredis.keys( "stats:*" )
       expect(count_keys.length).to eq(0)
     end
 
@@ -33,7 +39,7 @@ RSpec.describe Redbus::Stats do
       load_some_data
       yr = Date.today.year
       mo = Date.today.month
-      counts = Redbus::Stats.counts_for( "@test1" )
+      counts = @current_redbus.counts_for( "@test1" )
       expect(counts.length).to eq(3)
       expect(counts["published"][yr][mo]).to eq(1)
       expect(counts["processed"][yr][mo]).to eq(2)
@@ -43,12 +49,12 @@ RSpec.describe Redbus::Stats do
   end # stats
 
   def load_some_data
-    Redbus::Stats.bump( "@test1", "published" )
-    Redbus::Stats.bump( "@test1", "processed" )
-    Redbus::Stats.bump( "@test1", "processed" )
-    Redbus::Stats.bump( "@test1", "failed" )
-    Redbus::Stats.bump( "@test1", "failed" )
-    Redbus::Stats.bump( "@test1", "failed" )
+    @current_redbus.bump( "@test1", "published" )
+    @current_redbus.bump( "@test1", "processed" )
+    @current_redbus.bump( "@test1", "processed" )
+    @current_redbus.bump( "@test1", "failed" )
+    @current_redbus.bump( "@test1", "failed" )
+    @current_redbus.bump( "@test1", "failed" )
   end
 
 end
